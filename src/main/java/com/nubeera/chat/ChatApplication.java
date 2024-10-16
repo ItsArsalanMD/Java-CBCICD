a18662a8b463b3ad035d6edeb64310c0fbf2cee7
Ubuntu <ubuntu@ip-172-31-36-36.ec2.internal>
2024-10-16 05:04:57 +0000

    java project added

---
+++ src/main/java/com/nubeera/chat/ChatApplication.java
@@ -0,0 +1,14 @@
+package com.nubeera.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+		System.out.println("hello sir how are you!!");
+	}
+
+}
